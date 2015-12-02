665ce298111544e50b98ebf3ca0ed6e6f7e8cf90
Bryan Way <bryanwayb@gmail.com>
2015-12-01 22:20:21 -0600

    Added .js and .css minification
    Will need to add this to a common.js file when making new controllers

--- controllers/mvc.js
+++ controllers/mvc.js
@@ -2,7 +2,10 @@
 
 var path = require('path'),
     jshtml = require('js-html'),
-    fs = require('fs');
+    fs = require('fs'),
+    minify = require('html-minifier').minify,
+    uglifyjs = require('uglifyjs'),
+    CleanCSS = require('clean-css');
 
 function renderPartial(dir, partial, global, parent) {
     var file = path.resolve(dir, partial);
@@ -30,7 +33,15 @@ function renderPartial(dir, partial, global, parent) {
     return script;
 }
 
-var docsDir = path.resolve(__dirname, '../docs/mvc');
+var docsDir = path.resolve(__dirname, '../docs/mvc'),
+    htmlMinifyOptions = {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeAttributeQuotes: true,
+        useShortDoctype: true,
+        minifyJS: true,
+        minifyCSS: true
+    };
 var serverPostBuild = {
     'apache2': function(out) {
         fs.createReadStream(path.join(docsDir, '.htaccess')).pipe(fs.createWriteStream(path.join(out, '.htaccess')));
@@ -54,6 +65,14 @@ module.exports = {
         };
         var globalContext = { };
         parentContext.rendered = renderPartial(__dirname, params.source, globalContext, parentContext).render();
-        return renderPartial(__dirname, parentContext.layout, globalContext, parentContext).render();
+        return minify(renderPartial(__dirname, parentContext.layout, globalContext, parentContext).render(), htmlMinifyOptions);
+    },
+    '.js': function(params) {
+        var ast = uglifyjs.parse(fs.readFileSync(params.source).toString());
+        ast.figure_out_scope();
+        return ast.transform(uglifyjs.Compressor()).print_to_string();
+    },
+    '.css': function(params) {
+        return new CleanCSS().minify(fs.readFileSync(params.source).toString()).styles;
     }
 };
